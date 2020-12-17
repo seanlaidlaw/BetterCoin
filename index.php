f975712ab2395302efdd039e777ddd93e0b27bfd
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2020-12-17 12:48:31 +0000

    add more information to annonces including google maps link if coordinates

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 
 <div class="annonce-list">
-<h1>Annonces: <?php echo $_GET["motcle"]; ?> </h1>
+<h1>Annonces : <?php echo $_GET["motcle"]; ?> </h1>
 
 <?php
 $db = new SQLite3('annonces.db');
@@ -27,9 +27,25 @@ $res = $db->query($sqlQueryStr);
 
 while ($row = $res->fetchArray()) {
     echo '<div class="annonce">';
-    echo "  <h4 style='margin: 0;'>{$row['titre']} <span id='category'>[{$row['categorie']}]</span></h4>";
-    echo "  <div id=annonce-prix>{$row['prix']} €</div>";
-    echo "  <span>{$row['description']}</span>";
+    echo "  <div class='annonce-title-description-pseudo'>";
+    echo "    <div class='annonce-title'>";
+    echo "       <h4 style='margin: 0;'>{$row['titre']} <span id='category'>[{$row['categorie']}]</span></h4>";
+    echo "       <div class='annonce-description'>{$row['description']}</div>";
+    echo "    </div>";
+    echo "    <div class='annonce-pseudo'>{$row['pseudo']} — {$row['date']}</div>";
+    echo "  </div>";
+    echo "  <div class='annonce-img-prix-coord'>";
+    echo "    <div class='annonce-img-prix'>";
+    echo "      <div class=annonce-prix><p>{$row['prix']} €</p></div>";
+    echo "      <div class='annonce-img'><img style='max-height:160px;max-width:200px;' src={$row['photo']}></div>";
+    echo "    </div>";
+
+if(empty($row['rdv_lon'])){
+    echo "    <div class='annonce-coord'><p style='font-style: italic;'>No Location Data Provided</p></div>";
+} else {
+    echo "    <div class='annonce-coord'><a target=\"_blank\" href=\"http://maps.google.com/maps?q={$row['rdv_lat']},{$row['rdv_lon']}\">Localise (Google Maps)</a></div>";
+}
+    echo "  </div>";
     echo '</div>';
 }
 ?>
